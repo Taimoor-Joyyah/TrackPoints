c08e0d2ae4a100fd56ac699cd6f799a3ecb49ee1
mtaimoor <>
2022-05-22 14:20:54 +0500

    Print Track Points on Turtle

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import Point_Class
 from Point_Class import Point as Point2D, Point3D
+from turtle import *
 
 point_list = [
-    Point3D(2, 5, 7), Point3D(3, 7, 9), Point3D(9, 0, 3),
-    Point3D(3, 6, 5), Point3D(5, 5, 0), Point3D(1, 9, 3),
-    Point3D(4, 0, 1), Point3D(8, 8, 9), Point3D(0, 2, 0)
+    Point2D(2, 5), Point2D(3, 7), Point2D(9, 0),
+    Point2D(3, 6), Point2D(5, 5), Point2D(1, 9),
+    Point2D(4, 0), Point2D(8, 8), Point2D(0, 2)
 ]
 
 current_point = point_list[0]
@@ -17,9 +18,14 @@ while point_not_on_track:
     point_not_on_track.remove(current_point)
     point_on_track.append(current_point)
 
-print([(point.x, point.y, point.z) for point in point_on_track])
+print([(point.x, point.y) for point in point_on_track])
 
-print(point_on_track[0].distance(point_on_track[1]))
-print(point_on_track[1].distance(point_on_track[2]))
-print(point_on_track[2].distance(point_on_track[3]))
-print(point_on_track[3].distance(point_on_track[4]))
+# Printing Track Point on Turtle
+penup()
+ht()
+for i, point in enumerate(point_on_track):
+    goto(point.x*50, point.y*50)
+    if not i:
+        pendown()
+    dot()
+done()
